7b1fdd060479f6cf2182f9fc3bf1c3b6a75cb438
Vitaly Sinilin <unterwulf@users.sourceforge.net>
2010-03-09 14:22:04 +0300

    changed debug info format to be more compact

--- src/dump.c
+++ src/dump.c
@@ -4,17 +4,11 @@
 
 void dump_id3_header(const struct id3v2_header *hdr)
 {
-    print(OS_DEBUG,
-        "id3v2 tag info:\n"
-        "  version: %u.%u\n"
-        "  flags: %u\n"
-        "  size: %u",
+    print(OS_DEBUG, "id3v2 tag version: %u.%u, flags: %u, size: %u",
         hdr->version, hdr->revision, hdr->flags, hdr->size);
 }
 
 void dump_frame(const struct id3v2_frame *frame)
 {
-    print(OS_DEBUG,
-        "frame %.4s, len=%u",
-        frame->id, frame->size);
+    print(OS_DEBUG, "frame %.4s, len=%u", frame->id, frame->size);
 }
