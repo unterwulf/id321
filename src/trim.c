c78f0bfe4693cff434f57c6a0e76578031a105ce
Vitaly Sinilin <unterwulf@users.sourceforge.net>
2011-04-21 11:28:28 +0400

    Simplify trim_id3v1_tag()

--- src/trim.c
+++ src/trim.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <errno.h>
 #include <unistd.h> /* SEEK_* */
 #include <string.h>
@@ -5,74 +6,90 @@
 #include "id3v1.h"
 #include "output.h"
 #include "params.h" /* NOT_SET */
-#include "trim.h"
 #include "file.h"
+
+#define ID3V1E_TAG_EXTRA_SIZE (ID3V1E_TAG_SIZE - ID3V1_TAG_SIZE)
+#define ID3V12_TAG_EXTRA_SIZE (ID3V12_TAG_SIZE - ID3V1_TAG_SIZE)
+
+/***
+ * trim_trailing_chunk
+ *
+ * Trims a chunk of size @size from the end of crop area of @file
+ * if the chunk's first @hdr_sz bytes are equal to the ones @hdr is
+ * pointing to.
+ *
+ * Returns 0 if the chunk has been trimmed, or -ENOENT otherwise.
+ */
+
+static int trim_trailing_chunk(struct file *file, const char *hdr,
+                               size_t hdr_sz, size_t size)
+{
+    char buf[4];
+
+    assert(hdr_sz <= sizeof(buf));
+
+    if (file->crop.start + size <= file->crop.end)
+    {
+        lseek(file->fd, file->crop.end - size, SEEK_SET);
+        read(file->fd, buf, hdr_sz);
+
+        if (!memcmp(buf, hdr, hdr_sz))
+        {
+            file->crop.end -= size;
+            return 0;
+        }
+    }
+
+    return -ENOENT;
+}
 
 /***
  * trim_id3v1_tag - trim ID3v1 tag from @file
  *
- * Returns size of the tag trimmed or negative errno on error.
+ * Trims an ID3v1 tag of the specified version @minor from the end of crop
+ * area of @file.
+ *
+ * Returns size of the tag trimmed, or -ENOENT if the tag has not been found.
  */
 
 int trim_id3v1_tag(struct file *file, unsigned minor)
 {
-    char buf[4];
-    off_t orig_crop_end = file->crop.end;
-
     if (minor == 0 || minor == 1 || minor == 2 || minor == 3 ||
         minor == ID3V1E_MINOR || minor == NOT_SET)
     {
-        if (file->crop.start + ID3V1_TAG_SIZE > file->crop.end)
-            return -ENOENT;
+        off_t orig_crop_end = file->crop.end;
 
-        lseek(file->fd, file->crop.end - ID3V1_TAG_SIZE, SEEK_SET);
-        read(file->fd, buf, ID3V1_HEADER_SIZE);
-
-        if (!memcmp(buf, ID3V1_HEADER, ID3V1_HEADER_SIZE))
+        if (trim_trailing_chunk(file, ID3V1_HEADER, ID3V1_HEADER_SIZE,
+                                ID3V1_TAG_SIZE) == 0)
         {
             print(OS_INFO, "ID3v1 tag found");
-            file->crop.end -= ID3V1_TAG_SIZE;
-        }
-        else
-            return -ENOENT;
-    }
 
-    /* check presence of v1 enhanced tag */
-    if ((minor == ID3V1E_MINOR || minor == NOT_SET) &&
-        file->crop.end - (ID3V1E_TAG_SIZE - ID3V1_TAG_SIZE) >= file->crop.start)
-    {
-        lseek(file->fd, file->crop.end - (ID3V1E_TAG_SIZE - ID3V1_TAG_SIZE),
-              SEEK_SET);
-        read(file->fd, buf, ID3V1E_HEADER_SIZE);
+            /* check presence of v1 enhanced tag */
+            if ((minor == ID3V1E_MINOR || minor == NOT_SET) &&
+                trim_trailing_chunk(file, ID3V1E_HEADER, ID3V1E_HEADER_SIZE,
+                                    ID3V1E_TAG_EXTRA_SIZE) == 0)
+            {
+                print(OS_INFO, "ID3v1 enhanced tag found");
+                return ID3V1E_TAG_SIZE;
+            }
 
-        if (!memcmp(buf, ID3V1E_HEADER, ID3V1E_HEADER_SIZE))
-        {
-            print(OS_INFO, "ID3v1 enhanced tag found");
-            file->crop.end -= ID3V1E_TAG_SIZE - ID3V1_TAG_SIZE;
-            return ID3V1E_TAG_SIZE;
-        }
-    }
+            /* check presence of tag v1.2 */
+            if ((minor == 2 || minor == NOT_SET) &&
+                trim_trailing_chunk(file, ID3V12_HEADER, ID3V12_HEADER_SIZE,
+                                    ID3V12_TAG_EXTRA_SIZE) == 0)
+            {
+                print(OS_INFO, "ID3v1.2 tag found");
+                return ID3V12_TAG_SIZE;
+            }
 
-    /* check presence of tag v1.2 */
-    if ((minor == 2 || minor == NOT_SET) &&
-        file->crop.end - (ID3V12_TAG_SIZE - ID3V1_TAG_SIZE) >= file->crop.start)
-    {
-        lseek(file->fd, file->crop.end - (ID3V12_TAG_SIZE - ID3V1_TAG_SIZE),
-              SEEK_SET);
-        read(file->fd, buf, ID3V12_HEADER_SIZE);
+            if (minor == 0 || minor == 1 || minor == 3 || minor == NOT_SET)
+                return ID3V1_TAG_SIZE;
 
-        if (!memcmp(buf, ID3V12_HEADER, ID3V12_HEADER_SIZE))
-        {
-            print(OS_INFO, "ID3v1.2 tag found");
-            file->crop.end -= ID3V12_TAG_SIZE - ID3V1_TAG_SIZE;
-            return ID3V12_TAG_SIZE;
+            /* tag of the specified version has not been found */
+            file->crop.end = orig_crop_end;
         }
     }
 
-    if (minor == 0 || minor == 1 || minor == 3 || minor == NOT_SET)
-        return ID3V1_TAG_SIZE;
-
-    file->crop.end = orig_crop_end;
     return -ENOENT;
 }
 
